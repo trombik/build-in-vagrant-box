089f6b95b877a92e1a9638030c44f105dfc8bc86
Tomoyuki Sakurai <y@trombik.org>
2020-05-13 15:16:16 +0900

    feature: pass ENVVARS

--- bin/run.sh
+++ bin/run.sh
@@ -3,12 +3,14 @@
 set -e
 set -x
 
+MY_FILENAME=`realpath $0`
+MY_DIR=`dirname ${MY_FILENAME}`
+
 SRC_FILE=`realpath $1`
 SRC_FILE_BASENAME=`basename "${SRC_FILE}"`
 DEST_DIR="/home/vagrant/build"
 
-MY_FILENAME=`realpath $0`
-MY_DIR=`dirname ${MY_FILENAME}`
+ENVVARS=$2
 
 VAGRANT_CWD=`realpath ${MY_DIR}/..`
 export VAGRANT_CWD
@@ -17,4 +19,4 @@ vagrant status
 vagrant ssh --command "mkdir ${DEST_DIR}" default
 vagrant upload "${SRC_FILE}" "${DEST_DIR}/${SRC_FILE_BASENAME}" default
 vagrant ssh --command "ls -al ${DEST_DIR}" default
-vagrant ssh --command "sh -c \"(cd ${DEST_DIR} && chmod +x ${SRC_FILE_BASENAME} && ./${SRC_FILE_BASENAME})\"" default
+vagrant ssh --command "sh -c \"(cd ${DEST_DIR} && chmod +x ${SRC_FILE_BASENAME} && env ${ENVVARS} ./${SRC_FILE_BASENAME})\"" default
